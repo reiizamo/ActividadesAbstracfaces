86dbc77eb7e70d3b522e51fbd6e09c2693b37f9a
Asier Zamora <asierferzam@gmail.com>
2026-05-23 17:56:15 +0100

    metodo arrancar modificado

--- src/net/salesianos/actividades/actividad2/vehiculo/cocheCombustion/CocheCombustion.java
+++ src/net/salesianos/actividades/actividad2/vehiculo/cocheCombustion/CocheCombustion.java
@@ -2,13 +2,13 @@ package net.salesianos.actividades.actividad2.vehiculo.cocheCombustion;
 import net.salesianos.actividades.actividad2.vehiculo.Vehiculo;
 
 public class CocheCombustion extends Vehiculo {
-    private String motorCombustion = "encendido";
-    private double litrosCombustible;
+    private String motorCombustion = "apagado";
+    private int litrosCombustible;
 
     public CocheCombustion(){
     }
 
-    public CocheCombustion(String color, String modelo, String matricula, int numeroPuertas, int numeroRuedas, double litrosCombustible){
+    public CocheCombustion(String color, String modelo, String matricula, int numeroPuertas, int numeroRuedas, int litrosCombustible){
         super(color, modelo, matricula, numeroPuertas, numeroRuedas);
         this.litrosCombustible = litrosCombustible;
 
@@ -16,6 +16,14 @@ public class CocheCombustion extends Vehiculo {
 
     @Override
     public void arrancar() { 
+        if(this.litrosCombustible == 0){
+            System.out.print("No hay combustible en el motor.");
+            System.out.println("Motor: " + this.motorCombustion);
+        }else{
+            this.litrosCombustible = this.litrosCombustible - 1;
+            System.out.print("Motor: " + this.motorCombustion);
+            System.out.println("Un litro de combustible gastado.");
+        }
     }
     public void motor() {
     }
