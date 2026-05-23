2c137ab00ff7831e93cca55d6bb232cfbc9d1679
Asier Zamora <asierferzam@gmail.com>
2026-05-23 15:50:53 +0100

    atributos y constructores

---
+++ src/net/salesianos/actividades/actividad1/rectangulo/Rectangulo.java
@@ -0,0 +1,13 @@
+package net.salesianos.actividades.actividad1.rectangulo;
+
+public class Rectangulo {
+    private double altura;
+    private double ancho;
+
+    public Rectangulo(){
+    }
+    public Rectangulo(double altura, double ancho) {
+        this.altura = altura;
+        this.ancho = ancho;
+    }
+}
