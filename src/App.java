a761768eba0e5e8b0a97a9158126aa96b93fb099
Asier Zamora <asierferzam@gmail.com>
2026-05-23 18:20:20 +0100

    actividad dos, completada en el app

--- src/App.java
+++ src/App.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 import net.salesianos.actividades.actividad1.circulo.Circulo;
 import net.salesianos.actividades.actividad1.rectangulo.Rectangulo;
+import net.salesianos.actividades.actividad2.vehiculo.cocheCombustion.CocheCombustion;
 
 
 public class App {
     public static void main(String[] args) throws Exception {
+        // Actividad 1
         Scanner sc = new Scanner(System.in);
         System.out.println();
         System.out.println("Introduce, la altura y el ancho de un rectangulo:");
@@ -33,7 +35,29 @@ public class App {
         System.out.print("El circulo tiene un radio de: " + circulo.getRadio() + "\n");
         System.out.print("Tiene un área de: " + circulo.calcularArea() + "\n");
         System.out.println("Tiene un perímetro de: " + circulo.calcularPerimetro());
-
         sc.close();
+
+
+        // Actividad 2
+        CocheCombustion coche = new CocheCombustion("Rojo", "celica", "4587 TNF", 4, 4, 20);
+        System.out.println();
+        coche.mover();
+        System.out.println();
+
+        coche.arrancar();
+        System.out.println();
+
+        coche.mover();
+        coche.mover();
+        coche.mover();
+        System.out.println();
+
+        System.out.print("Tiene: " + coche.getLitros() + "L de combustible\n");
+
+        coche.setLitros(2);
+        coche.mover();
+        coche.mover();
+        coche.mover();
+
     }
 }
--- src/net/salesianos/actividades/actividad2/vehiculo/cocheCombustion/CocheCombustion.java
+++ src/net/salesianos/actividades/actividad2/vehiculo/cocheCombustion/CocheCombustion.java
@@ -17,29 +17,32 @@ public class CocheCombustion extends Vehiculo {
     public int getLitros(){
         return this.litrosCombustible;
     }
+    public void setLitros(int litros){
+        this.litrosCombustible = litros;
+    }
 
     @Override
     public void arrancar() { 
         if(this.litrosCombustible <= 0){
-            System.out.print("No hay combustible en el motor.");
+            System.out.print("No hay combustible en el motor.\n");
             this.motorCombustion = "apagado";
             System.out.println("Motor: " + this.motorCombustion);
         }else{
             this.litrosCombustible = this.litrosCombustible - 1;
             this.motorCombustion = "encendido";
-            System.out.print("Motor: " + this.motorCombustion);
+            System.out.print("Motor: " + this.motorCombustion + "\n");
             System.out.println("Un litro de combustible gastado.");
         }
     }
     public void pararMotor() {
         this.motorCombustion = "apagado";
-        System.out.print("Motor: " + this.motorCombustion);
+        System.out.println("Motor: " + this.motorCombustion);
     }
     public void mover() {
         if(this.motorCombustion.equals("apagado")){
             System.out.println("El motor esta: " + this.motorCombustion);
         }else if(this.litrosCombustible <= 0){
-            System.out.print("No hay combustible en el motor.");
+            System.out.print("No hay combustible en el motor.\n");
             this.motorCombustion = "apagado";
             System.out.println("Motor: " + this.motorCombustion);
         }else {
