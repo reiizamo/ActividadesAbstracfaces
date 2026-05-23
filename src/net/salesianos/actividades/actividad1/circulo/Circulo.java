881403f01db1f95b15d0a434dc3b55cffecebe59
Asier Zamora <asierferzam@gmail.com>
2026-05-23 15:57:51 +0100

    atributos y constructores

---
+++ src/net/salesianos/actividades/actividad1/circulo/Circulo.java
@@ -0,0 +1,12 @@
+package net.salesianos.actividades.actividad1.circulo;
+
+public class Circulo {
+    private double radio;
+
+    public Circulo(){
+    }
+
+    public Circulo(double radio){
+        this.radio = radio;
+    }
+}
