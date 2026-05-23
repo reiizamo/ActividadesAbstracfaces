98384c4ee39b510df18ed74596ed9a84f9e75560
Asier Zamora <asierferzam@gmail.com>
2026-05-23 16:01:55 +0100

    implementacion de la interfaz

--- src/net/salesianos/actividades/actividad1/circulo/Circulo.java
+++ src/net/salesianos/actividades/actividad1/circulo/Circulo.java
@@ -1,6 +1,7 @@
 package net.salesianos.actividades.actividad1.circulo;
+import net.salesianos.actividades.actividad1.calculoGeometrico.CalculoGeometrico;
 
-public class Circulo {
+public class Circulo implements CalculoGeometrico{
     private double radio;
 
     public Circulo(){
@@ -8,5 +9,16 @@ public class Circulo {
 
     public Circulo(double radio){
         this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        double resultado = Math.PI * Math.pow(this.radio, 2);
+        return resultado;
+    }
+
+    public double calcularPerimetro() {
+        double resultado = 2 * Math.PI * this.radio;
+        return resultado;
     }
 }
