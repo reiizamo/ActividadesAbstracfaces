c0ffab32deaf3d09dfc0f29cc6356de76aeb2b88
Asier Zamora <asierferzam@gmail.com>
2026-05-23 15:54:38 +0100

    implementacion de la interfaz

--- src/net/salesianos/actividades/actividad1/rectangulo/Rectangulo.java
+++ src/net/salesianos/actividades/actividad1/rectangulo/Rectangulo.java
@@ -1,6 +1,7 @@
 package net.salesianos.actividades.actividad1.rectangulo;
+import net.salesianos.actividades.actividad1.calculoGeometrico.CalculoGeometrico;
 
-public class Rectangulo {
+public class Rectangulo implements CalculoGeometrico{
     private double altura;
     private double ancho;
 
@@ -9,5 +10,16 @@ public class Rectangulo {
     public Rectangulo(double altura, double ancho) {
         this.altura = altura;
         this.ancho = ancho;
+    }
+
+    @Override
+    public double calcularArea() {
+        double resultado = this.ancho * this.altura;
+        return resultado;
+    }
+
+    public double calcularPerimetro() {
+        double resultado = 2 * (this.ancho + this.altura);
+        return resultado;
     }
 }
